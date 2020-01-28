8d733f396ac9a206be6cfe4ca56a710f70e7eaa4
Hasan Su'udi <koetik@gmail.com>
2020-01-28 09:59:43 +0700

    data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#app',
     data:{
         name :'Genclik',
-        job : 'koplak'
+        job : 'koplak',
+        website : 'https://github.com/koetik/',
+        websiteTag : '<a href="https://github.com/koetik/"> website on v-html</a>'
     },
     methods : {
         greet : function(time) {
