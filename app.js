bef28e8cb2def01ddfa3a46b2543c250e04e7fea
Hasan Su'udi <koetik@gmail.com>
2020-01-28 10:37:29 +0700

    pelajaran 2 event

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
         name :'Genclik',
         job : 'koplak',
         website : 'https://github.com/koetik/',
-        websiteTag : '<a href="https://github.com/koetik/"> website on v-html</a>'
+        websiteTag : '<a href="https://github.com/koetik/"> website on v-html</a>',
+        age : 20,
+        x : 0,
+        y : 0,
     },
     methods : {
         greet : function(time) {
             return 'Selamat '+ time 
+        },
+        add : function(val) {
+            this.age += val
+        },
+        substract : function(val) {
+            this.age -= val
+        },
+        updateXY : function(event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
         }
     }
 });
