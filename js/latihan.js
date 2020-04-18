12fd2d29a1939a09c8bfd32b91ccf3a8f3a687c0
Hasan Su'udi <koetik@gmail.com>
2020-04-18 08:31:30 +0700

    latihan 1

---
+++ js/latihan.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data:{
+        name : '',
+        genders : {
+        	selected : '',
+        	option : [
+	        	{key : 'male', value : 'Laki-laki'},
+	        	{key : 'female', value : 'Perempuan'},
+	        ]	
+        }
+    },
+    methods : {
+        
+    },
+    computed : {
+        
+    }
+});
