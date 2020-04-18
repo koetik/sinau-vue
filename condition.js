9aa41913c7b14a1c929b4c7d40ca044de4e21ac6
Hasan Su'udi <koetik@gmail.com>
2020-04-18 08:31:05 +0700

    Looping

--- condition.js
+++ condition.js
@@ -3,6 +3,21 @@ new Vue({
     data:{
         error : false,
         success : false,
+        characters : ['barking', 'tuyul ijo', 'panah', 'hiler'],
+        goldSaints : [
+        	{nama : 'Mu', bintang : 'Aries'},
+        	{nama : 'Aldebaran', bintang : 'Taurus'},
+        	{nama : 'Saga', bintang : 'Gemini'},
+        	{nama : 'Milo', bintang : 'Scorpio'},
+        	{nama : 'Deathmask', bintang : 'Cancer'},
+        	{nama : 'Aiolia', bintang : 'Leo'},
+        	{nama : 'Aiolos', bintang : 'Sagitarius'},
+        	{nama : 'Aphrodite', bintang : 'Pisces'},
+        	{nama : 'Shura', bintang : 'Capricorn'},
+        	{nama : 'Shaka', bintang : 'Virgo'},
+        	{nama : 'Camus', bintang : 'Aquarius'},
+        	{nama : 'Douko', bintang : 'Libra'},
+        ]
     },
     methods : {
         
