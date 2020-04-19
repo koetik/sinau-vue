72a2b1e4764520dd7a1fb8097348ba032f3e64df
Hasan Su'udi <koetik@gmail.com>
2020-04-19 08:22:59 +0700

    latihan 2

--- js/latihan.js
+++ js/latihan.js
@@ -8,12 +8,44 @@ new Vue({
 	        	{key : 'male', value : 'Laki-laki'},
 	        	{key : 'female', value : 'Perempuan'},
 	        ]	
+        },
+        profile : {
+        	nama : '',
+        	tinggi : '',
+        	berat : '',
+        	warnaMata : '',
+        	makananFavorit : '',
+        },
+        calc : {
+        	a : 0,
+        	b : 0,
+        	res : 0,
+        	operator : '+',
         }
     },
     methods : {
         
     },
     computed : {
-        
+        calculate : function() {
+        	switch (this.calc.operator) {
+        		case '+':
+        			this.calc.res = this.calc.a + this.calc.b;
+        			break;
+    			case '-':
+    				this.calc.res = this.calc.a - this.calc.b;
+        			break;
+    			case '*':
+    				this.calc.res = this.calc.a * this.calc.b;
+        			break;
+    			case '/':
+    				if (this.calc.b != 0) {
+    					this.calc.res = this.calc.a / this.calc.b;
+    				} else {
+    					this.calc.res = 'Error';	
+    				}
+        			break;
+        	}
+        }
     }
 });
